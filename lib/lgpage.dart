47c4c9cbac317a86ecc4f162fb44447e41beb693
vatiza <rakibur.mac@gmail.com>
2023-11-18 16:30:15 +0600

    first commit

---
+++ lib/lgpage.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class loginpage extends StatefulWidget {
+  const loginpage({super.key});
+
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text("data"),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    'BUBT Help Desk',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          suffixIcon: Icon(Icons.email),
+                          labelText: "Email"),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          suffixIcon: Icon(Icons.password),
+                          labelText: "Password"),
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      
+                        onPressed: null,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    
+                        ),
+                    ) 
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextButton(
+                      onPressed: null, child: Text('Forget your password')),
+                ],
+              ),
+              Column(
+                children: [
+                  TextButton(
+                      onPressed: null, child: Text('Create an New Account'))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
